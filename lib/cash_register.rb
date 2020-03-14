93661df2b16e58fcb212157ad3fa0f230585905d
taroj khan <tarujkhan@yahoo.com>
2020-03-14 15:11:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    final_value = price * quantity
+    @final_value = price * quantity
     if quantity > 1
       counter = 0
       while counter < quantity
